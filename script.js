cc43459a614d358eb287fde49c5be060a923ef14
EmiBP <emillybritopinheiro@gmail.com>
2024-07-23 19:43:32 +0200

    .

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-const active = document.querySelector('.active');
+
+
+
 
 window.addEventListener('load', function() {
-
-   
-    active.style.fontSize = '14.08px';
+    
+    adicionarElementos()
    
     
 })
+
+
+
+function adicionarElementos() {
+    const header = document.querySelector('header');
+
+    
+    header.innerHTML = `
+    <div class="header_nav">
+        <nav>
+            <ul>
+                <li><a  href="index.html">Home</a></li>
+                <li><a  href="projects.html">Projects</a></li>
+                <li><a  href="about.html">About</a></li>
+                <li><a  href="CV.html">CV</a></li>
+            </ul>
+        </nav>
+    </div>
+ `
+
+}
+
+
+
+
+   
+
+
+
 
 
